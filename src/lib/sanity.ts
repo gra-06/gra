b251cf86e8734855fa3b03fd411539b65f802439
Mustafa Saraçoğlu <mustafa.saracoglu11@gmail.com>
2025-08-23 23:19:15 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import { createClient } from 'next-sanity';
 
 export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-const apiVersion = '2023-05-03';
+export const apiVersion = '2023-05-03';
 
 if (!projectId) {
   throw new Error('The `NEXT_PUBLIC_SANITY_PROJECT_ID` environment variable is not set.');
